ff90d6b6ecff23b3bc7052bd6ba0cb1d79b1d4eb
Kate Sergeyenko <ekaterina.sergeyenko@gmail.com>
2017-10-04 19:16:29 +0300

    Tic-tac-toe implementation.

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,98 @@
+const SIZE_OF_FIELD = 3;
+
 class TicTacToe {
-    constructor() {
+  constructor() {
+		this.currentSymbol = 'x';
+		this.matrix = [
+					[null, null, null],
+					[null, null, null],
+					[null, null, null],
+				];
+  }
 
-    }
+	getCurrentPlayerSymbol() {
+		return  this.currentSymbol;
+	}
 
-    getCurrentPlayerSymbol() {
+	nextTurn(rowIndex, columnIndex) {
+		if (this.getFieldValue(rowIndex, columnIndex) === null && this.isFinished() === false) {
+			this.matrix[rowIndex][columnIndex] = this.currentSymbol;
 
-    }
+			this.currentSymbol = this.getCurrentPlayerSymbol() === 'x' ? 'o' : 'x';
+		}
+	}
 
-    nextTurn(rowIndex, columnIndex) {
+	isFinished() {
+		return this.getWinner() !== null || this.noMoreTurns();
+	}
 
-    }
+	getWinner() {
+		const arr = new Array(8);
 
-    isFinished() {
+		for (let i = 0; i < 8; i++) {
+			arr[i] = [];
+		}
 
-    }
+		let k = 0;
+		for (let i = 0; i < SIZE_OF_FIELD; i++) {
+			for (let j = 0; j < SIZE_OF_FIELD; j++) {
+				arr[i][j] = this.matrix[i][j];
+				arr[i + SIZE_OF_FIELD][j] = this.matrix[j][i];
+			}
+			k += 2;
+		}
+		
+		for (let j = 0; j < SIZE_OF_FIELD; j++) {
+			arr[k][j] = this.matrix[j][j];
+			arr[k + 1][j] = this.matrix[j][2 - j];
+		}
 
-    getWinner() {
+		////////////////////////////////////////////////
+		let isXWinner = false;
+		let isOWinner = false;
 
-    }
+		for (k = 0; k < 8; k++) {
+			if (arr[k][0] === arr[k][1] && arr[k][0] === arr[k][2]) {
+				if (arr[k][0] === 'x') {
+					isXWinner = true;
+				}
+				else if (arr[k][0] === 'o') {
+					isOWinner = true;
+				}
+			}
+		}
+		/////////////////////////////////////////////////
 
-    noMoreTurns() {
+		if (isXWinner === true && isOWinner === false) {
+			return 'x';
+		}
+		else if (isOWinner === true && isXWinner === false) {
+			return 'o';
+		}
+		else {
+			return null;
+		}
+	}
 
-    }
+	noMoreTurns() {
+		for (var i = 0; i < SIZE_OF_FIELD; i++) {
+			for (var j = 0; j < SIZE_OF_FIELD; j++) {
+				if (this.matrix[i][j] === null){
+					return false;
+				}
+			}
+		}
 
-    isDraw() {
+		return true;
+	}
 
-    }
+	isDraw() {
+		return this.isFinished() && this.getWinner() === null;
+	}
 
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+	getFieldValue(rowIndex, colIndex) {
+		return this.matrix[rowIndex][colIndex];
+	}
 }
 
 module.exports = TicTacToe;
